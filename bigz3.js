4e2367e8edad76ca90b79b319093989b8b0c7f95
Voyager-06 <mihailbalabanuk@gmail.com>
2026-08-19 17:12:47 +0700

    Add files via upload

---
+++ bigz3.js
@@ -0,0 +1,6 @@
+let a = prompt('Input your... 1+1 :) ');
+let b = confirm(eval(a)+'. Further?');
+while (b==true){
+    a = prompt('Input your... 1+1 :) ');
+    b = confirm(eval(a)+'. Further?');
+    }
