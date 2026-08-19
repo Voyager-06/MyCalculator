d831cfe7958aa98c12d0c1844f0c0b1212ed53b1
Voyager-06 <mihailbalabanuk@gmail.com>
2026-08-19 17:26:11 +0700

    Update MiniCalcOnJS.js
    Add 2nd lang

--- MiniCalcOnJS.js
+++ MiniCalcOnJS.js
@@ -1,7 +1,7 @@
 //работает на любых (почти) сайтах кроме гитхаба, т.к. на нём security politic не даёт сделать eval()
-let a = prompt('Input your... 1+1 :) ');
-let b = confirm(eval(a)+'. Further?');
+let a = prompt('Введите выражение: / Input: ');
+let b = confirm(eval(a)+'. Дальше? / Further?');
 while (b==true){
-    a = prompt('Input your... 1+1 :) ');
-    b = confirm(eval(a)+'. Further?');
+    a = prompt('Введите выражение: / Input: ');
+    b = confirm(eval(a)+'. Дальше? / Further?');
     }
